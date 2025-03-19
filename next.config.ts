69259f168c1f3254d241303966e73659ae36d8e4
lightstudios4u <lightstudios4u@gmail.com>
2025-03-19 13:53:35 -0600

    Removed .env.local from commit

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: ".env.local" });
 const nextConfig = {
   images: {
     domains: ["resonancestorage.sfo2.digitaloceanspaces.com"], // ✅ Allow DigitalOcean Spaces
